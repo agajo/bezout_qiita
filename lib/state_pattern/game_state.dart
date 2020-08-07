c4213d34472da285960aa898ead4c3135e270a66
agajo <madaopc@gmail.com>
2020-08-08 00:25:41 +0900

    making StateNotifier+freezed pattern. WIP

---
+++ lib/state_pattern/game_state.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'game_state.freezed.dart';
+
+@freezed
+abstract class GameState with _$GameState {
+  factory GameState({
+    @Default(0) int a,
+    @Default(0) int b,
+    @Default(0) int x,
+    @Default(0) int y,
+    @Default(0) int target,
+    @Default(0) int clearedCount,
+    @Default(0) int pressedCount,
+    @Default(false) bool justCleared,
+  }) = _GameState;
+}
